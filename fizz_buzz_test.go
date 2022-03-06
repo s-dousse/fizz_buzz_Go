2dc33951aeada22340d89eeff4274722d4dd4c10
Sarah <dousse.sarah@gmail.com>
2022-03-06 15:29:05 +0000

    refactoring

--- fizz_buzz_test.go
+++ fizz_buzz_test.go
@@ -3,21 +3,24 @@ package main
 import "testing"
 
 func TestFizzBuzz(t *testing.T) {
+	assertErrorMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("it returns 'Fizz' when passed 3", func(t *testing.T) {
 		got := FizzBuzz(3)
 		want :=  "Fizz"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertErrorMessage(t, got, want)
 	})
 
 	t.Run("it returns 'FizzBuzz' when passed 15", func(t *testing.T) {
 		got := FizzBuzz(15)
 		want :=  "FizzBuzz"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertErrorMessage(t, got, want)
 	})
 }
